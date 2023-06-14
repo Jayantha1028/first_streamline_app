686d065ce16f66dfa866b71e41b3e5b6a1418e74
Jayantha1028 <70044284+Jayantha1028@users.noreply.github.com>
2023-06-14 13:37:28 +0530

    Update streamlit_app.py
    Customize option

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale, spinach & Rocket smoothie')
-streamlit.text('Hard-boiled Free-Range Egg') 
+
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text('🥗 Kale, spinach & Rocket smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range Egg') 
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
