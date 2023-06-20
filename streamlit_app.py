7071170b7255f3f539bd8d32f05704c5617f1287
Jayantha1028 <70044284+Jayantha1028@users.noreply.github.com>
2023-06-20 20:16:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('🥣 Omega 3 & blueberry oatmeal')
 streamlit.text('🥗 Kale, spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range Egg') 
@@ -24,16 +22,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected= streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
 streamlit.dataframe(fruityvice_normalized)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
