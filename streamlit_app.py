29e332b6140b932a8928850254d2d41334d69645
Jayantha1028 <70044284+Jayantha1028@users.noreply.github.com>
2023-06-21 19:56:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,3 +83,22 @@ if streamlit.button('Add a fruit to list'):
   streamlit.text(back_from_function)
  
 streamlit.stop()
+
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+ 
+
+#add_my_fruit=
+
+ 
+
+#add a second text box
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+ 
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
